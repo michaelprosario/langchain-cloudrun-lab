07106740201c81a335ce86667b42a3674c0c968c
Michael Rosario <michael.p.rosario@gmail.com>
2024-09-15 12:39:23 +0000

    feat ( main ) got virtual spock working

--- main.py
+++ main.py
@@ -2,50 +2,88 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-import getpass
+
 import os
-import bs4
+
+from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+google_api_key = os.environ["GOOGLE_API_KEY"]
+
+llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=google_api_key)
+
+
+from fastapi import FastAPI
 from langchain import hub
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_google_vertexai import ChatVertexAI
+from langserve import add_routes
 
-#os.environ["GOOGLE_API_KEY"] = getpass.getpass()
 
-llm = ChatVertexAI(model="gemini-1.5-flash")
+# # Load, chunk and index the contents of the blog.
+# loader = WebBaseLoader(
+#     web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+#     bs_kwargs=dict(
+#         parse_only=bs4.SoupStrainer(
+#             class_=("post-content", "post-title", "post-header")
+#         )
+#     ),
+# )
+# docs = loader.load()
 
-# Load, chunk and index the contents of the blog.
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=bs4.SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
-        )
-    ),
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+# splits = text_splitter.split_documents(docs)
+# embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+# vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+# # Retrieve and generate using the relevant snippets of the blog.
+# retriever = vectorstore.as_retriever()
+# prompt = hub.pull("rlm/rag-prompt")
+
+# def format_docs(docs):
+#     return "\n\n".join(doc.page_content for doc in docs)
+
+system_template = "Respond as Mr. Spock from Star Trek:"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
 )
-docs = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
-vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-
-# Retrieve and generate using the relevant snippets of the blog.
-retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
-
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
 
 rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
+    prompt_template
     | llm
     | StrOutputParser()
 )
 
-rag_chain.invoke("What is Task Decomposition?")
+from fastapi import FastAPI
+from langserve import add_routes
+
+# App definition
+app = FastAPI(
+  title="LangChain Server",
+  version="1.0",
+  description="A simple API server using LangChain's Runnable interfaces",
+)
+
+# Adding chain route
+add_routes(
+    app,
+    rag_chain,
+    path="/chain",
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
+
+# user_input = ""
+# while user_input != "exit":
+#     user_input = input(">")
+#     output = rag_chain.invoke(user_input)
+#     print(output)
+    
