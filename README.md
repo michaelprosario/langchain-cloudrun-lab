601e8d7505b373de051d10c9b454cdf832e57a4f
Michael Rosario <michael.p.rosario@gmail.com>
2024-09-18 02:35:32 +0000

    Add Dockerfile and requirements.txt for containerization

--- README.md
+++ README.md
@@ -3,3 +3,24 @@
 # reference
 - https://python.langchain.com/v0.2/docs/tutorials/rag/
 - https://www.digitalocean.com/community/tutorials/how-to-install-and-use-sqlite-on-ubuntu-20-04
+
+
+# How do we build the docker container and name the image langchain-lab1?
+
+```
+docker build -t langchain-lab1 .
+```
+
+# How do we run an instance of langchain-lab1?
+
+```
+docker run -d -p 8000:8000 -e GOOGLE_API_KEY="fixme" -e LANGCHAIN_TRACING_V2=true -e LANGCHAIN_ENDPOINT="https://api.smith.langchain.com" -e LANGCHAIN_API_KEY="fixme" langchain-lab1
+```
+
+* -d: This flag runs the container in detached mode, meaning it runs in the background.
+* -p 8000:8000: This option maps port 80 of the host machine to port 80 of the container. This allows access to the container's services via port 80 on the host machine.
+* -e GOOGLE_API_KEY="fixme": This sets an environment variable GOOGLE_API_KEY inside the container with the value "fixme".
+* -e LANGCHAIN_TRACING_V2=true: This sets an environment variable LANGCHAIN_TRACING_V2 inside the container with the value true.
+* -e LANGCHAIN_ENDPOINT="https://api.smith.langchain.com": This sets an environment variable LANGCHAIN_ENDPOINT inside the container with the value "https://api.smith.langchain.com".
+* -e LANGCHAIN_API_KEY="fixme": This sets an environment variable LANGCHAIN_API_KEY inside the container with the value "fixme".
+* langchain-lab1: This is the name of the Docker image to use for creating the container.
--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 
 import os
 
@@ -11,7 +7,6 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 google_api_key = os.environ["GOOGLE_API_KEY"]
 
 llm = GoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=google_api_key)
-
 
 from fastapi import FastAPI
 from langchain import hub
@@ -24,30 +19,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langserve import add_routes
-
-
-# # Load, chunk and index the contents of the blog.
-# loader = WebBaseLoader(
-#     web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-#     bs_kwargs=dict(
-#         parse_only=bs4.SoupStrainer(
-#             class_=("post-content", "post-title", "post-header")
-#         )
-#     ),
-# )
-# docs = loader.load()
-
-# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-# splits = text_splitter.split_documents(docs)
-# embeddings=GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-# vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
-
-# # Retrieve and generate using the relevant snippets of the blog.
-# retriever = vectorstore.as_retriever()
-# prompt = hub.pull("rlm/rag-prompt")
-
-# def format_docs(docs):
-#     return "\n\n".join(doc.page_content for doc in docs)
 
 system_template = "Respond as Mr. Spock from Star Trek:"
 prompt_template = ChatPromptTemplate.from_messages(
